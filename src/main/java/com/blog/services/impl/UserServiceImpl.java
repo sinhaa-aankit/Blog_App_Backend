a6e9dd771d3adf2541a4c551bd1a6e92a75f8d7d
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-05-08 11:35:33 +0530

    Created User Api

---
+++ src/main/java/com/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,70 @@
+package com.blog.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.blog.entities.User;
+import com.blog.payloads.UserDto;
+import com.blog.repositories.UserRepo;
+import com.blog.services.UserService;
+
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		User user = this.dtoToUser(userDto);
+		User savedUser = this.userRepo.save(user);
+		return this.userToDto(savedUser);
+	}
+
+	@Override
+	public UserDto updateUser(UserDto user, Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto getUserById(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer userId) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private User dtoToUser(UserDto userDto) {
+		
+		User user = new User();
+		user.setId(userDto.getId());
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setAbout(userDto.getAbout());
+		user.setPassword(userDto.getPassword());
+		return user;
+		
+	}
+	
+	private UserDto userToDto(User user) {
+		UserDto userDto = new UserDto();
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setEmail(user.getEmail());
+		userDto.setAbout(user.getAbout());
+		userDto.setPassword(user.getPassword());
+		return userDto;
+	}
+
+}
